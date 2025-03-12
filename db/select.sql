7dc1c4ef616655a1a34e2bb99b78ff5fb27db054
Rion Takebayashi <6051_2023_05517@g.miyazaki-c.ed.jp>
2025-03-12 05:13:16 +0000

    テーブル選択

--- db/select.sql
+++ db/select.sql
@@ -1,5 +1,8 @@
 -- 中学校テーブルからすべてのデータを抽出する
 -- select * from schools;
 
--- 生徒テーブル（students)からすべてのデータ(*)を抽出する
-select * from students;
+-- 生徒テーブル（students)から性別が'女'のデータ(name, gender, address)を抽出する
+-- select name,gender,address from students where gender='女';
+
+-- 生徒テーブル（students)から性別が'女'のデータ(name, gender, address)を抽出する
+SELECT name,gender,school_name FROM students LEFT JOIN schools ON students.school_id = schools.school_id
